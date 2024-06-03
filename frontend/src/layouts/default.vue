fbfbc7973bf43db050d3e58a101969c9e15e6660
Ivan Riazanov <omega19961@gmail.com>
2024-06-03 22:08:08 +0600

    initial vuetify app

---
+++ frontend/src/layouts/default.vue
@@ -0,0 +1,14 @@
+<template>
+  <v-app>
+    <v-toolbar title="Application"></v-toolbar>
+
+    <v-main>
+      <router-view />
+    </v-main>
+
+  </v-app>
+</template>
+
+<script lang="ts" setup>
+//
+</script>
---
+++ frontend/src/pages/index.vue
@@ -0,0 +1,9 @@
+<template>
+  <v-container>
+    
+  </v-container>
+</template>
+
+<script lang="ts" setup>
+  //
+</script>
