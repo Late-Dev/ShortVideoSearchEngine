6049481c4220344ad2fab7eb37a97f61a5e06906
Ivan Riazanov <omega19961@gmail.com>
2024-06-04 10:39:02 +0600

    player position

--- frontend/src/pages/index.vue
+++ frontend/src/pages/index.vue
@@ -1,9 +1,24 @@
 <template>
   <v-container>
-    <video></video>
+    <div>
+
+      <video class="video" autoplay controls
+        src="https://cdn-st.rutubelist.ru/media/87/43/b11df3f344d0af773aac81e410ee/fhd.mp4"></video>
+    </div>
   </v-container>
 </template>
 
 <script lang="ts" setup>
 //
 </script>
+
+<style scoped lang="scss">
+.video {
+  position: absolute;
+  z-index: 0;
+  width: 100%;
+  height: calc(100% - 64px);
+  inset: 0px;
+  object-fit: cover;
+}
+</style>
