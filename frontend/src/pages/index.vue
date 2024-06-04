7cdb770c0d05b3e5b1497bfc0de3877d06f59ea8
Ivan Riazanov <omega19961@gmail.com>
2024-06-04 11:29:13 +0600

    added simple video scrolling

--- frontend/src/pages/index.vue
+++ frontend/src/pages/index.vue
@@ -1,15 +1,65 @@
 <template>
   <v-container>
     <div>
+      <div class="caption">{{ videoArray[activeVideo] }} {{ activeVideoData.description }}</div>
 
-      <video class="video" autoplay controls
-        src="https://cdn-st.rutubelist.ru/media/87/43/b11df3f344d0af773aac81e410ee/fhd.mp4"></video>
+      <video @wheel="onScrollWheel" v-touch="{
+        up: up,
+        down: down
+      }" class="video" autoplay controls :src="activeVideoData.link"></video>
     </div>
   </v-container>
 </template>
 
 <script lang="ts" setup>
-//
+import { computed, ref } from 'vue';
+import data from '../assets/yappy_hackaton_2024_400k.json'
+
+
+
+const videoSize = 400000
+
+const videoArray = ref<number[]>([generateRandomInt(videoSize)])
+
+const activeVideo = ref(0)
+
+function up() {
+  if (activeVideo.value > videoArray.value.length - 2) {
+
+    videoArray.value.push(generateRandomInt(videoSize))
+  }
+  activeVideo.value++
+}
+
+function down() {
+  if (activeVideo.value > 0) {
+
+    // videoArray.value.pop()
+    activeVideo.value--
+  }
+}
+
+function onScrollWheel(e) {
+  if (e.wheelDelta < 0) {
+    up()
+
+  }
+  else {
+    down()
+  }
+}
+
+
+
+
+function generateRandomInt(max) {
+  return Math.floor(Math.random() * (max)); // The maximum is exclusive and the minimum is inclusive
+}
+
+const activeVideoData = computed(() => {
+  return data[videoArray.value[activeVideo.value]]
+})
+
 </script>
 
 <style scoped lang="scss">
@@ -20,5 +70,11 @@
   height: calc(100% - 64px);
   inset: 0px;
   object-fit: cover;
+}
+
+.caption {
+  position: fixed;
+  z-index: 100;
+  bottom: 150px;
 }
 </style>
