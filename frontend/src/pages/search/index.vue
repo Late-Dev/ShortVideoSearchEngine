cad7663b06d2c745638d00f406ee32221d5aa10a
Ivan Riazanov <omega19961@gmail.com>
2024-06-16 00:11:13 +0600

    added autocomplete

--- frontend/src/pages/search/index.vue
+++ frontend/src/pages/search/index.vue
@@ -1,22 +1,17 @@
 <template>
   <v-container>
-    <v-text-field
-      v-model="search"
-      label="Поиск"
-      prepend-inner-icon="mdi-magnify"
-      variant="outlined"
-      hide-details
-      single-line
-      density="compact"
-      color="primary"
-      @input="debouncedGetSearch"
-    ></v-text-field>
-    <v-virtual-scroll item-height="'calc(50vh)'" :height="'calc(100vh - 45px)'"
- :items="videos" >
-      <template  v-slot:default="{ item }">
+    <!-- <v-text-field v-model="search" label="Поиск" prepend-inner-icon="mdi-magnify" variant="outlined" hide-details
+      single-line density="compact" color="primary" @input="debouncedGetSearch"></v-text-field> -->
+    <v-autocomplete :items="words" variant="outlined" density="compact" item-props hide-no-data label="Поиск"
+      prepend-inner-icon="mdi-magnify" @update:search="search = $event" theme="dark" @update:model-value="getSearch"
+      @keydown.enter="getSearch"></v-autocomplete>
+    <v-virtual-scroll item-height="'calc(50vh)'" :height="'calc(100vh - 45px)'" :items="videos">
+      <template v-slot:default="{ item }">
         <div style="display: flex; position: relative; justify-content: center;">
           <video controls :src="item.link" :key="item.link" style="height: 50vh"></video>
-          <div style=" position: absolute; bottom: 80px; text-align: center; background: rgba(0, 0, 0, 0.5); border-radius: 10px">{{ item.description }}</div>
+          <div
+            style=" position: absolute; bottom: 80px; text-align: center; background: rgba(0, 0, 0, 0.5); border-radius: 10px">
+            {{ item.description }}</div>
 
         </div>
       </template>
@@ -34,6 +29,8 @@ import { ref } from "vue";
 import { getVideosSearch } from "../../api";
 import { Videos } from "../../types";
 
+import words from './all_words.json'
+
 const search = ref();
 
 const videos = ref<Videos[]>();
@@ -41,7 +38,7 @@ const videos = ref<Videos[]>();
 // Функция debounce
 function debounce(func, delay) {
   let timeoutId;
-  return function(...args) {
+  return function (...args) {
     if (timeoutId) {
       clearTimeout(timeoutId);
     }
